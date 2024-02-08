727f1999e93b067739c6484d3b96622cf3bedc0d
Jeff Chen <jch00078@gmail.com>
2024-02-08 16:55:31 -0500

    created custom login page with dripdrop logo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
-import { Button, View, withAuthenticator } from "@aws-amplify/ui-react";
+import { Button, View, Image, useTheme, withAuthenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from '@aws-amplify/ui-react';
 import awsconfig from "./amplifyconfiguration.json";
 import UploadImage from "./ui-components/UploadImage";
+import Logo from "./ui-components/logo.png";
 
 Amplify.configure(awsconfig);
-
+/*
 const App = ({ signOut }) => {
   return (
     <View className="App">
@@ -16,5 +18,33 @@ const App = ({ signOut }) => {
     </View>
   );
 };
+*/
+const components = {
+  Header() {
+    const { tokens } = useTheme();
 
-export default withAuthenticator(App);
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="DripDrop Logo"
+          src={Logo}
+        />
+      </View>
+    );
+  }
+};
+
+export default function App() {
+  return (
+    <Authenticator components={components}>
+      {({ signOut, user }) => (
+        <View className="App">
+          <UploadImage />
+          <Button onClick={signOut}>Sign Out</Button>
+        </View>
+      )}
+    </Authenticator>
+  );
+}
+
+//export default withAuthenticator(App);
