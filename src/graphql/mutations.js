535b6baa9024a9ff78befc652860de2d326e6102
Justin Choi <choij3163@gmail.com>
2024-02-21 13:59:41 -0500

    Update schema and codgen, some auto-tweaks to existing ui

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,6 +1,96 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const createFriendRequest = /* GraphQL */ `
+  mutation CreateFriendRequest(
+    $input: CreateFriendRequestInput!
+    $condition: ModelFriendRequestConditionInput
+  ) {
+    createFriendRequest(input: $input, condition: $condition) {
+      id
+      UserId
+      SenderId
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateFriendRequest = /* GraphQL */ `
+  mutation UpdateFriendRequest(
+    $input: UpdateFriendRequestInput!
+    $condition: ModelFriendRequestConditionInput
+  ) {
+    updateFriendRequest(input: $input, condition: $condition) {
+      id
+      UserId
+      SenderId
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteFriendRequest = /* GraphQL */ `
+  mutation DeleteFriendRequest(
+    $input: DeleteFriendRequestInput!
+    $condition: ModelFriendRequestConditionInput
+  ) {
+    deleteFriendRequest(input: $input, condition: $condition) {
+      id
+      UserId
+      SenderId
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const createFriend = /* GraphQL */ `
+  mutation CreateFriend(
+    $input: CreateFriendInput!
+    $condition: ModelFriendConditionInput
+  ) {
+    createFriend(input: $input, condition: $condition) {
+      id
+      UserId
+      FriendId
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateFriend = /* GraphQL */ `
+  mutation UpdateFriend(
+    $input: UpdateFriendInput!
+    $condition: ModelFriendConditionInput
+  ) {
+    updateFriend(input: $input, condition: $condition) {
+      id
+      UserId
+      FriendId
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteFriend = /* GraphQL */ `
+  mutation DeleteFriend(
+    $input: DeleteFriendInput!
+    $condition: ModelFriendConditionInput
+  ) {
+    deleteFriend(input: $input, condition: $condition) {
+      id
+      UserId
+      FriendId
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
 export const createPost = /* GraphQL */ `
   mutation CreatePost(
     $input: CreatePostInput!
---
+++ src/ui-components/PostActionCenter.d.ts
@@ -0,0 +1,29 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { ButtonProps, FlexProps } from "@aws-amplify/ui-react";
+export declare type EscapeHatchProps = {
+    [elementHierarchy: string]: Record<string, unknown>;
+} | null;
+export declare type VariantValues = {
+    [key: string]: string;
+};
+export declare type Variant = {
+    variantValues: VariantValues;
+    overrides: EscapeHatchProps;
+};
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type PostActionCenterOverridesProps = {
+    PostActionCenter?: PrimitiveOverrideProps<FlexProps>;
+    "Report Button"?: PrimitiveOverrideProps<ButtonProps>;
+    "View Profile Button"?: PrimitiveOverrideProps<ButtonProps>;
+    Button?: PrimitiveOverrideProps<ButtonProps>;
+} & EscapeHatchProps;
+export declare type PostActionCenterProps = React.PropsWithChildren<Partial<FlexProps> & {
+    overrides?: PostActionCenterOverridesProps | undefined | null;
+}>;
+export default function PostActionCenter(props: PostActionCenterProps): React.ReactElement;
