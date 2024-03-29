dca4205378e55845d793581875eae437d1147aa0
Surya Salem <salem9@purdue.edu>
2024-03-28 21:55:21 -0400

    amplify pulled

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -4,10 +4,19 @@
  * Any changes to this file will be overwritten when running amplify pull. *
  **************************************************************************/
 
+export { default as CommentComponent } from "./CommentComponent";
+export { default as DripDropNavBarBasic } from "./DripDropNavBarBasic";
+export { default as EditProfile } from "./EditProfile";
+export { default as FormCheckout } from "./FormCheckout";
+export { default as Friend } from "./Friend";
 export { default as FriendCreateForm } from "./FriendCreateForm";
+export { default as FriendRequest } from "./FriendRequest";
 export { default as FriendRequestCreateForm } from "./FriendRequestCreateForm";
 export { default as FriendRequestUpdateForm } from "./FriendRequestUpdateForm";
+export { default as FriendRequestsListCondensed } from "./FriendRequestsListCondensed";
 export { default as FriendUpdateForm } from "./FriendUpdateForm";
+export { default as FriendsList } from "./FriendsList";
+export { default as FullFriends } from "./FullFriends";
 export { default as MyIcon } from "./MyIcon";
 export { default as NotificationsCreateForm } from "./NotificationsCreateForm";
 export { default as NotificationsUpdateForm } from "./NotificationsUpdateForm";
@@ -17,6 +26,9 @@ export { default as PostCreateForm } from "./PostCreateForm";
 export { default as PostReportCreateForm } from "./PostReportCreateForm";
 export { default as PostReportUpdateForm } from "./PostReportUpdateForm";
 export { default as PostUpdateForm } from "./PostUpdateForm";
+export { default as ReportPost } from "./ReportPost";
 export { default as SavedPostsCreateForm } from "./SavedPostsCreateForm";
 export { default as SavedPostsUpdateForm } from "./SavedPostsUpdateForm";
+export { default as UserSearchRescind } from "./UserSearchRescind";
+export { default as UserSearchSend } from "./UserSearchSend";
 export { default as studioTheme } from "./studioTheme";
