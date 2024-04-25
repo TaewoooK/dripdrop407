7eafc3ee62fc03d6d8b89ccd19347a55a6c28b19
cheelingjet <lchee@purdue.edu>
2024-04-25 17:34:37 -0400

    Leaderboard updates

--- src/components/Leaderboard/Board.js
+++ src/components/Leaderboard/Board.js
@@ -14,6 +14,8 @@ import {
     onDeletePost
 } from "../../graphql/subscriptions"
 
+import BoardProfile from "./LeaderProfile";
+
 const client = generateClient();
 
 // DATE CONSTANTS
@@ -31,9 +33,9 @@ const endOfDay = new Date(
 
 export default function Board() {
   const { allUsers } = useContext(UserContext);
-  const [user, setSelectedUser] = useState(); 
+  const [user, setSelectedUser] = useState();
   const [selectedData, setSelectedData] = useState([]); // Initialize with an empty array
-
+  const [toggleUser, setUser] = useState(false);
   const [time, setTime] = useState("alltime");
   const [boardState, setBoardState] = useState("");
 
@@ -160,6 +162,7 @@ export default function Board() {
 
       // Convert the posts data into the desired format
       const convertedData = posts.map((post, index) => ({
+        toShow: post.description,
         username: post.owner,
         totalDripPoints: post.drip_points,
         rank: index + 1, // Adding 1 to make the ranks start from 1
@@ -176,16 +179,18 @@ export default function Board() {
     const userDripPointsList = [];
   
     for (const user of allUsers) {
-      const username = user.Username;
-      try {
-        const totalDripPoints = await getTotalDripPointsForUser(username);
-        userDripPointsList.push({ username, totalDripPoints });
-      } catch (error) {
-        console.error(
-          `Error fetching drip points for user ${username}:`,
-          error
-        );
-      }
+        const username = user.Username;
+        try {
+          const totalDripPoints = await getTotalDripPointsForUser(username);
+          userDripPointsList.push({
+            toShow: user.Username, // Adjust as needed
+            username: user.Username,
+            totalDripPoints: totalDripPoints,
+            rank: 0, // Rank will be assigned later
+          });
+        } catch (error) {
+          console.error(`Error fetching drip points for user ${username}:`, error);
+        }
     }
   
     // Sort the list by totalDripPoints in descending order
@@ -194,48 +199,109 @@ export default function Board() {
     // Assign ranks to each user
     userDripPointsList.forEach((user, index) => {
       user.rank = index + 1; // Adding 1 to make the ranks start from 1
-      user.rank = index + 1; // Adding 1 to make the ranks start from 1
     });
   
     return userDripPointsList;
   }
 
   useEffect(() => {
-    setSelectedData([]);
-  }, [allUsers]);
+    const fetchData = async () => {
+        const data = await getAllPostsRankedByDripPoints();
+        setBoardState("byPost");
+    };
+    fetchData();
+}, []); // empty dependency array ensures the effect runs only once on mount
+
+  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+//   const PostModal = ({ onClose }) => {
+//     return (
+//       <div style={modalContainerStyles}>
+//         <div style={modalStyles}>
+//           <h1>PUT CONTENT HERE</h1>
+//           <button onClick={onClose}>Close</button>
+//         </div>
+//       </div>
+//     );
+//   };
+
+  //const user = allUsers.find((user) => user.Username === username); 
+  const modalContainerStyles = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // semi-transparent black background
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    boxSizing: "border-box", // Ensure padding is included in the width and height
+    overflow: "auto", // Enable scrolling if the content overflows the viewport
+    zIndex: 9999, // higher z-index to ensure it's above other content
+  };
+  
+  const modalStyles = {
+    backgroundColor: "rgba(34, 34, 34, 1)",
+    padding: "20px",
+    borderRadius: "8px",
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+  };
+
+  const PostModal = ({ onClose }) => {
+    return (
+      <div style={modalContainerStyles}>
+        <div style={modalStyles}>
+          <BoardProfile user={user} />
+          <button onClick={onClose}>Close</button>
+        </div>
+      </div>
+    );
+  };
+
+  const openModal = (username) => {
+    const user = allUsers.find((user) => user.Username === username); // Find the user by username
+    setSelectedUser(allUsers.find((user) => user.Username === username)); // Set the user
+    setIsModalOpen(true); // Open the modal
+  };
+  
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+
 
   return (
     <View className="leaderboard-container">
       <View className="board">
         <View className="board2">
           <Text className="rank-text" children="Rank" />
-          <Text className="user-text" children="User" />
+          <Text className="user-text" children={toggleUser ? "User" : "Post"} />
           <Text className="point-text" children="Points" />
           <hr className="separator" />
-          <div className="columns-container" style={{ overflow: "auto" }}>
-            {/* First, add a div to contain the columns */}
-            <View className="rank-column">
-              {selectedData.map((entry, index) => (
-                <h2 key={index} style={{ color: "white" }}>
-                  {entry.rank}
-                </h2>
-              ))}
-            </View>
-            <View className="user-column">
-              {selectedData.map((entry, index) => (
-                <h2 key={index} style={{ color: "white" }}>
-                  {entry.username}
-                </h2>
-              ))}
-            </View>
-            <View className="points-column">
-              {selectedData.map((entry, index) => (
-                <h2 key={index} style={{ color: "white" }}>
-                  {entry.totalDripPoints}
-                </h2>
-              ))}
-            </View>
-          </div>
+          <div className="columns-container" style={{ cursor: "pointer", overflow: "auto" }} 
+                        >
+                      <View className="rank-column">
+                          {selectedData.map((entry, index) => (
+                              <h2 key={index} onClick={() => openModal(entry.username)} style={{ color: "white" }}>{entry.rank}</h2>
+                          ))}
+                      </View>
+                      <View className="user-column">
+                          {selectedData.map((entry, index) => (
+                              <h2 key={index} onClick={() => openModal(entry.username)} style={{ color: "white" }}>{entry.toShow}</h2>
+                          ))}
+                      </View>
+                      <View className="points-column">
+                          {selectedData.map((entry, index) => (
+                              <h2 key={index} onClick={() => openModal(entry.username)} style={{ color: "white" }}>{entry.totalDripPoints}</h2>
+                          ))}
+                      </View>
+            </div>
+            <div>
+                {isModalOpen && <PostModal onClose={closeModal} />}
+            </div>
         </View>
         <Text className="board-header-text">
           <span style={{ color: "#047d95" }}>Leader</span>
